b9785796ba88519bcfc0394bc52182ba8bcf7e52
Danilo Scano <daniloscano@gmail.com>
2025-06-28 22:35:01 +0200

    added validation for guest

--- backend/modules/guest/guest.route.js
+++ backend/modules/guest/guest.route.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const guestController = require('./guest.controller')
+const { createValidationRules, updateValidationRules, guestValidator } = require('./guestValidation')
 
 const guest = express.Router()
 
 guest.get("/", guestController.getAlGuests)
 guest.get("/:guestId", guestController.getGuestById)
-guest.post("/create", guestController.createGuest)
-guest.patch("/:guestId/edit", guestController.updateGuestById)
+guest.post("/create", [ createValidationRules, guestValidator ], guestController.createGuest)
+guest.patch("/:guestId/edit", [ updateValidationRules, guestValidator ], guestController.updateGuestById)
 guest.delete("/:guestId/delete", guestController.deleteGuestById)
 
 module.exports = guest

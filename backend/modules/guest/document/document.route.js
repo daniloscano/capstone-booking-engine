eb3ab73d99e08404539698ada9f60afcddce9eae
Danilo Scano <daniloscano@gmail.com>
2025-06-29 01:40:01 +0200

    added validation for document

--- backend/modules/guest/document/document.route.js
+++ backend/modules/guest/document/document.route.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const documentController = require('./document.controller')
+const { createValidationRules, updateValidationRules, documentValidator } = require('./documentValidation')
 
 const document = express.Router()
 
 document.get("/", documentController.getAllDocuments)
 document.get("/:documentId", documentController.getDocumentById)
-document.post("/create", documentController.createDocument)
-document.patch("/:documentId/edit", documentController.updateDocumentById)
+document.post("/create", [ createValidationRules, documentValidator ], documentController.createDocument)
+document.patch("/:documentId/edit", [ updateValidationRules, documentValidator ], documentController.updateDocumentById)
 document.delete("/:documentId/delete", documentController.deleteDocumentById)
 
 module.exports = document

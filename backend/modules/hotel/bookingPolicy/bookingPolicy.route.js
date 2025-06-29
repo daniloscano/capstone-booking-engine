248c3cb909320dfc3644287379874772a031ce5c
Danilo Scano <daniloscano@gmail.com>
2025-06-29 21:20:32 +0200

    added validation for bookingPolicy

--- backend/modules/hotel/bookingPolicy/bookingPolicy.route.js
+++ backend/modules/hotel/bookingPolicy/bookingPolicy.route.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const bookingPolicyController = require('./bookingPolicy.controller')
+const { createValidationRules, updateValidationRules, bookingPolicyValidator } = require('./bookingPolicyValidation')
 
 const bookingPolicy = express.Router()
 
 bookingPolicy.get("/", bookingPolicyController.getAllBookingPolicies)
 bookingPolicy.get("/:bookingPolicyId", bookingPolicyController.getBookingPolicyById)
-bookingPolicy.post("/create", bookingPolicyController.createBookingPolicy)
-bookingPolicy.patch("/:bookingPolicyId/edit", bookingPolicyController.updateBookingPolicyById)
+bookingPolicy.post("/create", [ createValidationRules, bookingPolicyValidator ], bookingPolicyController.createBookingPolicy)
+bookingPolicy.patch("/:bookingPolicyId/edit", [ updateValidationRules, bookingPolicyValidator ], bookingPolicyController.updateBookingPolicyById)
 bookingPolicy.delete("/:bookingPolicyId/delete", bookingPolicyController.deleteBookingPolicyById)
 
 module.exports = bookingPolicy

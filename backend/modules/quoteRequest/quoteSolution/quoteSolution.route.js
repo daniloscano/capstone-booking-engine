2f03fad1eed1cdc057634c7e7e86aaa4de745cb3
Danilo Scano <daniloscano@gmail.com>
2025-06-30 00:58:21 +0200

    added validation for quoteSolution

--- backend/modules/quoteRequest/quoteSolution/quoteSolution.route.js
+++ backend/modules/quoteRequest/quoteSolution/quoteSolution.route.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const quoteSolutionController = require('./quoteSolution.controller')
+const { createValidationRules, updateValidationRules, quoteSolutionValidator } = require('./quoteSolutionValidation')
 
 const quoteSolution = express.Router()
 
 quoteSolution.get("/", quoteSolutionController.getAllQuoteSolutions)
 quoteSolution.get("/:quoteSolutionId", quoteSolutionController.getQuoteSolutionById)
-quoteSolution.post("/create", quoteSolutionController.createQuoteSolution)
-quoteSolution.patch("/:quoteSolutionId/edit", quoteSolutionController.updateQuoteSolutionById)
+quoteSolution.post("/create", [ createValidationRules, quoteSolutionValidator ], quoteSolutionController.createQuoteSolution)
+quoteSolution.patch("/:quoteSolutionId/edit", [ updateValidationRules, quoteSolutionValidator ], quoteSolutionController.updateQuoteSolutionById)
 quoteSolution.delete("/:quoteSolutionId/delete", quoteSolutionController.deleteQuoteSolutionById)
 
 module.exports = quoteSolution

fba62c7c1702deae9549ec3767851c77352a4136
Danilo Scano <daniloscano@gmail.com>
2025-07-02 01:23:38 +0200

    added validation for booking

--- backend/modules/booking/booking.route.js
+++ backend/modules/booking/booking.route.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const bookingController = require('./booking.controller')
+const { createValidationRules, updateValidationRules, bookingValidator } = require('./bookingValidation')
 
 const booking = express.Router()
 
 booking.get("/", bookingController.getAllBookings)
 booking.get("/:bookingId", bookingController.getBookingById)
-booking.post("/create", bookingController.createBooking)
-booking.patch("/:bookingId/edit", bookingController.updateBookingById)
+booking.post("/create", [ createValidationRules, bookingValidator ], bookingController.createBooking)
+booking.patch("/:bookingId/edit", [ updateValidationRules, bookingValidator ], bookingController.updateBookingById)
 booking.delete("/:bookingId/delete", bookingController.deleteBookingById)
 
 module.exports = booking

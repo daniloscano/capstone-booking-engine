9c5a6106402251fb25874ba068b13f9ea7c6d8ef
Danilo Scano <daniloscano@gmail.com>
2025-07-08 01:46:30 +0200

    added rbac for roomType routes

--- backend/modules/room/roomType/roomType.route.js
+++ backend/modules/room/roomType/roomType.route.js
@@ -2,13 +2,31 @@ const express = require('express')
 const roomTypeController = require('./roomType.controller')
 const { createValidationRules, updateValidationRules, roomTypeValidator } = require('./roomTypeValidation')
 const { roomsUpload } = require('@middlewares/multer/index')
+const authMiddleware = require("@authMiddlewares/auth")
+const authorizedRoles = require('@authMiddlewares/role')
 
 const roomType = express.Router()
 
 roomType.get("/", roomTypeController.getAllRoomTypes)
 roomType.get("/:roomTypeId", roomTypeController.findRoomTypeById)
-roomType.post("/create", [ roomsUpload().array('images', 5), createValidationRules, roomTypeValidator ], roomTypeController.createRoomType)
-roomType.patch("/:roomTypeId/edit", [ roomsUpload().array('images', 5), updateValidationRules, roomTypeValidator ], roomTypeController.updateRoomTypeById)
-roomType.delete("/:roomTypeId/delete", roomTypeController.deleteRoomTypeById)
+roomType.post("/create",
+    authMiddleware,
+    authorizedRoles('admin', 'manager', 'operator'),
+    roomsUpload().array('images', 5),
+    [ createValidationRules, roomTypeValidator ],
+    roomTypeController.createRoomType
+)
+roomType.patch("/:roomTypeId/edit",
+    authMiddleware,
+    authorizedRoles('admin', 'manager', 'operator'),
+    roomsUpload().array('images', 5),
+    [ updateValidationRules, roomTypeValidator ],
+    roomTypeController.updateRoomTypeById
+)
+roomType.delete("/:roomTypeId/delete",
+    authMiddleware,
+    authorizedRoles('admin', 'manager', 'operator'),
+    roomTypeController.deleteRoomTypeById
+)
 
 module.exports = roomType

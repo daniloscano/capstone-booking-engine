adf9c6e4358a683acc286ea9f6d0e74d42aabb1c
Danilo Scano <daniloscano@gmail.com>
2025-07-08 01:53:40 +0200

    added rbac for roomAmenity routes

--- backend/modules/room/roomAmenity/roomAmenity.route.js
+++ backend/modules/room/roomAmenity/roomAmenity.route.js
@@ -2,13 +2,39 @@ const express = require('express')
 const roomAmenityController = require('./roomAmenity.controller')
 const { createValidationRules, updateValidationRules, roomAmenityValidator } = require('./roomAmenityValidation')
 const { iconsUpload } = require('@middlewares/multer/index')
+const authMiddleware = require("@authMiddlewares/auth")
+const authorizedRoles = require('@authMiddlewares/role')
 
 const roomAmenity = express.Router()
 
-roomAmenity.get("/", roomAmenityController.getAllRoomAmenities)
-roomAmenity.get("/:roomAmenityId", roomAmenityController.getRoomAmenityById)
-roomAmenity.post("/create", [ iconsUpload().single('icon'), createValidationRules, roomAmenityValidator ], roomAmenityController.createRoomAmenity)
-roomAmenity.patch("/:roomAmenityId/edit", [ iconsUpload().single('icon'), updateValidationRules, roomAmenityValidator ], roomAmenityController.updateRoomAmenityById)
-roomAmenity.delete("/:roomAmenityId/delete", roomAmenityController.deleteRoomAmenityById)
+roomAmenity.get("/",
+    authMiddleware,
+    authorizedRoles('admin', 'manager', 'operator'),
+    roomAmenityController.getAllRoomAmenities
+)
+roomAmenity.get("/:roomAmenityId",
+    authMiddleware,
+    authorizedRoles('admin', 'manager', 'operator'),
+    roomAmenityController.getRoomAmenityById
+)
+roomAmenity.post("/create",
+    authMiddleware,
+    authorizedRoles('admin', 'manager', 'operator'),
+    iconsUpload().single('icon'),
+    [ createValidationRules, roomAmenityValidator ],
+    roomAmenityController.createRoomAmenity
+)
+roomAmenity.patch("/:roomAmenityId/edit",
+    authMiddleware,
+    authorizedRoles('admin', 'manager', 'operator'),
+    iconsUpload().single('icon'),
+    [ updateValidationRules, roomAmenityValidator ],
+    roomAmenityController.updateRoomAmenityById
+)
+roomAmenity.delete("/:roomAmenityId/delete",
+    authMiddleware,
+    authorizedRoles('admin', 'manager', 'operator'),
+    roomAmenityController.deleteRoomAmenityById
+)
 
 module.exports = roomAmenity

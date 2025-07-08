2f3ce26631b3204e7c7adc335ae7069dc8fb9a10
Danilo Scano <daniloscano@gmail.com>
2025-07-08 02:20:12 +0200

    added rbac for booking routes

--- backend/modules/booking/booking.route.js
+++ backend/modules/booking/booking.route.js
@@ -1,14 +1,39 @@
 const express = require('express')
 const bookingController = require('./booking.controller')
 const { createValidationRules, updateValidationRules, bookingValidator } = require('./bookingValidation')
+const authMiddleware = require("@authMiddlewares/auth")
+const authorizedRoles = require('@authMiddlewares/role')
 
 const booking = express.Router()
 
-booking.get("/", bookingController.getAllBookings)
-booking.get("/:bookingId", bookingController.getBookingById)
-booking.get("/guest/:masterGuestId", bookingController.getBookingsByMasterGuestId)
-booking.post("/create", [ createValidationRules, bookingValidator ], bookingController.createBooking)
-booking.patch("/:bookingId/edit", [ updateValidationRules, bookingValidator ], bookingController.updateBookingById)
-booking.delete("/:bookingId/delete", bookingController.deleteBookingById)
+booking.get("/",
+    authMiddleware,
+    authorizedRoles('admin', 'manager', 'operator'),
+    bookingController.getAllBookings
+)
+booking.get("/:bookingId",
+    authMiddleware,
+    bookingController.getBookingById
+)
+booking.get("/guest/:masterGuestId",
+    authMiddleware,
+    bookingController.getBookingsByMasterGuestId
+)
+booking.post("/create",
+    authMiddleware,
+    authorizedRoles('admin', 'manager', 'operator'),
+    [ createValidationRules, bookingValidator ],
+    bookingController.createBooking
+)
+booking.patch("/:bookingId/edit",
+    authMiddleware,
+    [ updateValidationRules, bookingValidator ],
+    bookingController.updateBookingById
+)
+booking.delete("/:bookingId/delete",
+    authMiddleware,
+    authorizedRoles('admin', 'manager', 'operator'),
+    bookingController.deleteBookingById
+)
 
 module.exports = booking

fd6d273e87839ab98d9a78e9b97181d6bfa5fcf1
Danilo Scano <daniloscano@gmail.com>
2025-07-08 02:16:05 +0200

    added rbac for address routes

--- backend/modules/guest/address/address.route.js
+++ backend/modules/guest/address/address.route.js
@@ -1,13 +1,32 @@
 const express = require('express')
 const addressController = require('./address.controller')
 const { createValidationRules, updateValidationRules, addressValidator } = require('./addressValidation')
+const authMiddleware = require("@authMiddlewares/auth")
+const authorizedRoles = require('@authMiddlewares/role')
 
 const address = express.Router()
 
-address.get("/", addressController.getAllAddresses)
-address.get("/:addressId", addressController.getAddressById)
-address.post("/create", [ createValidationRules, addressValidator ], addressController.createAddress)
-address.patch("/:addressId/edit", [ updateValidationRules, addressValidator ], addressController.updateAddressById)
-address.delete("/:addressId/delete", addressController.deleteAddressById)
+address.get("/",
+    authMiddleware,
+    addressController.getAllAddresses
+)
+address.get("/:addressId",
+    authMiddleware,
+    addressController.getAddressById
+)
+address.post("/create",
+    authMiddleware,
+    [ createValidationRules, addressValidator ],
+    addressController.createAddress
+)
+address.patch("/:addressId/edit",
+    authMiddleware,
+    [ updateValidationRules, addressValidator ],
+    addressController.updateAddressById
+)
+address.delete("/:addressId/delete",
+    authMiddleware,
+    addressController.deleteAddressById
+)
 
 module.exports = address

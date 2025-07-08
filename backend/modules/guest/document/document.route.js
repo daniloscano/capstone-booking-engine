76a40562930a73241ea2cbbae8f2c7dfec68957c
Danilo Scano <daniloscano@gmail.com>
2025-07-08 02:14:47 +0200

    added rbac for document routes

--- backend/modules/guest/document/document.route.js
+++ backend/modules/guest/document/document.route.js
@@ -1,13 +1,32 @@
 const express = require('express')
 const documentController = require('./document.controller')
 const { createValidationRules, updateValidationRules, documentValidator } = require('./documentValidation')
+const authMiddleware = require("@authMiddlewares/auth")
+const authorizedRoles = require('@authMiddlewares/role')
 
 const document = express.Router()
 
-document.get("/", documentController.getAllDocuments)
-document.get("/:documentId", documentController.getDocumentById)
-document.post("/create", [ createValidationRules, documentValidator ], documentController.createDocument)
-document.patch("/:documentId/edit", [ updateValidationRules, documentValidator ], documentController.updateDocumentById)
-document.delete("/:documentId/delete", documentController.deleteDocumentById)
+document.get("/",
+    authMiddleware,
+    documentController.getAllDocuments
+)
+document.get("/:documentId",
+    authMiddleware,
+    documentController.getDocumentById
+)
+document.post("/create",
+    authMiddleware,
+    [ createValidationRules, documentValidator ],
+    documentController.createDocument
+)
+document.patch("/:documentId/edit",
+    authMiddleware,
+    [ updateValidationRules, documentValidator ],
+    documentController.updateDocumentById
+)
+document.delete("/:documentId/delete",
+    authMiddleware,
+    documentController.deleteDocumentById
+)
 
 module.exports = document

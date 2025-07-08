37c41ee4d9c565e8cf49e354a66f35388fa60194
Danilo Scano <daniloscano@gmail.com>
2025-07-08 02:12:31 +0200

    added rbac for guest routes

--- backend/modules/guest/guest.route.js
+++ backend/modules/guest/guest.route.js
@@ -1,13 +1,32 @@
 const express = require('express')
 const guestController = require('./guest.controller')
 const { createValidationRules, updateValidationRules, guestValidator } = require('./guestValidation')
+const authMiddleware = require("@authMiddlewares/auth")
+const authorizedRoles = require('@authMiddlewares/role')
 
 const guest = express.Router()
 
-guest.get("/", guestController.getAllGuests)
-guest.get("/:guestId", guestController.getGuestById)
-guest.post("/create", [ createValidationRules, guestValidator ], guestController.createGuest)
-guest.patch("/:guestId/edit", [ updateValidationRules, guestValidator ], guestController.updateGuestById)
-guest.delete("/:guestId/delete", guestController.deleteGuestById)
+guest.get("/",
+    authMiddleware,
+    guestController.getAllGuests
+)
+guest.get("/:guestId",
+    authMiddleware,
+    guestController.getGuestById
+)
+guest.post("/create",
+    authMiddleware,
+    authorizedRoles('admin', 'manager', 'operator'),
+    [ createValidationRules, guestValidator ], guestController.createGuest
+)
+guest.patch("/:guestId/edit",
+    authMiddleware,
+    [ updateValidationRules, guestValidator ], guestController.updateGuestById
+)
+guest.delete("/:guestId/delete",
+    authMiddleware,
+    authorizedRoles('admin', 'manager', 'operator'),
+    guestController.deleteGuestById
+)
 
 module.exports = guest

61f263ea66e5fdb49094094f4d6bee59e7129db7
Danilo Scano <daniloscano@gmail.com>
2025-07-09 22:06:11 +0200

    added two columns section to website homepage

--- frontend/src/pages/website/WebsiteHomepage.jsx
+++ frontend/src/pages/website/WebsiteHomepage.jsx
@@ -1,11 +1,24 @@
 import WebsiteBaseLayout from "../../layouts/website/WebsiteBaseLayout.jsx";
 import Hero from "../../components/hero/Hero.jsx";
+import TwoColsSection from "../../components/twoColsSection/TwoColsSection.jsx";
+import {roomsSection, servicesSection} from "../../components/twoColsSection/partials/sections.js";
 
 const WebsiteHomepage = () => {
     return (
         <>
             <WebsiteBaseLayout>
-                <Hero />
+                <main>
+                    <Hero/>
+                    <TwoColsSection
+                        section={roomsSection}
+                        imagePosition="left"
+                    />
+                    <TwoColsSection
+                        section={servicesSection}
+                        imagePosition="right"
+                    />
+                </main>
+
             </WebsiteBaseLayout>
         </>
     );

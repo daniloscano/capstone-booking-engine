d6fd3b26b2e034a4a5d436ac2f8a0543aa84869f
Danilo Scano <daniloscano@gmail.com>
2025-07-11 21:22:13 +0200

    added rooms container in rooms page

---
+++ frontend/src/components/roomsContainer/RoomsContainer.jsx
@@ -0,0 +1,30 @@
+import useRoomsStore from "../../stores/useRoomsStore.js";
+import {useRoomTypes} from "../../hooks/useRoomTypes.js";
+import {useEffect} from "react";
+
+const RoomsContainer = () => {
+    const { rooms, loading, error } = useRoomsStore()
+    const { getRoomTypes } = useRoomTypes()
+
+    useEffect(() => {
+        getRoomTypes()
+    }, []);
+
+    return (
+        <>
+            <div className="container">
+                <div className="row">
+                    {
+                        !loading && !error && rooms.map((room, index) => (
+                            <div key={`rooms-${index}`} className="col col-12 col-md-4 col-lg-3">
+                                <p>{room.category}</p>
+                            </div>
+                        ))
+                    }
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default RoomsContainer;
--- frontend/src/pages/website/RoomsPage.jsx
+++ frontend/src/pages/website/RoomsPage.jsx
@@ -1,10 +1,11 @@
 import WebsiteBaseLayout from "../../layouts/website/WebsiteBaseLayout.jsx";
+import RoomsContainer from "../../components/roomsContainer/RoomsContainer.jsx";
 
 const RoomsPage = () => {
     return (
         <>
             <WebsiteBaseLayout>
-
+                <RoomsContainer />
             </WebsiteBaseLayout>
         </>
     );

1ebda4a4c4e7d147cbe429977b3447fba0af2e97
Danilo Scano <daniloscano@gmail.com>
2025-07-12 00:51:42 +0200

    minor change: added href to sections

--- frontend/src/components/twoColsSection/partials/sections.js
+++ frontend/src/components/twoColsSection/partials/sections.js
@@ -2,12 +2,14 @@ export const roomsSection = {
     title: 'CAMERE',
     description: 'Scopri il comfort delle nostre camere',
     image: '/rooms-section.png',
-    alt: 'camere'
+    alt: 'camere',
+    href: '/rooms'
 }
 
 export const servicesSection = {
     title: 'SERVIZI',
     description: 'Scopri i servizi che offriamo ai nostri ospiti',
     image: '/services-section.png',
-    alt: 'servizi'
+    alt: 'servizi',
+    href: '/services'
 }

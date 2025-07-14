e63095f1e19b2f097ca4485097974c737fdfa75f
Danilo Scano <daniloscano@gmail.com>
2025-07-14 23:52:21 +0200

    add pageHeader on rooms page

--- frontend/src/pages/website/RoomsPage.jsx
+++ frontend/src/pages/website/RoomsPage.jsx
@@ -1,12 +1,17 @@
-import WebsiteBaseLayout from "../../layouts/website/WebsiteBaseLayout.jsx";
+import WebsitePagesLayout from "../../layouts/website/WebsitePagesLayout.jsx";
 import RoomsContainer from "../../components/roomsContainer/RoomsContainer.jsx";
+import { roomsPage } from "../../components/pageHeader/partials/pages.js"
+import PageHeader from "../../components/pageHeader/PageHeader.jsx";
 
 const RoomsPage = () => {
     return (
         <>
-            <WebsiteBaseLayout>
+            <WebsitePagesLayout>
+                <PageHeader
+                    page={roomsPage}
+                />
                 <RoomsContainer />
-            </WebsiteBaseLayout>
+            </WebsitePagesLayout>
         </>
     );
 };

03c3237c32317c941980ec2889596054f1685e6f
Danilo Scano <daniloscano@gmail.com>
2025-07-15 18:09:26 +0200

    add style to calendar on dates selection

--- frontend/src/components/bookingEngineSection/partials/BookingEngineForm.jsx
+++ frontend/src/components/bookingEngineSection/partials/BookingEngineForm.jsx
@@ -1,22 +1,33 @@
-import './bookingEngineForm.css'
 import {Calendar} from "primereact/calendar";
+import './bookingEngineForm.css'
+import {useState} from "react";
 
 const BookingEngineForm = () => {
 
+    const stayInputChange = (e) => {
+    }
 
+    const adultsChange = (e) => {
+    }
+
+    const onSubmit = (e) => {
+        e.preventDefault()
+    }
 
     return (
         <>
-            <form className="row align-items-center p-3 booking-engine-form">
+            <form
+                onSubmit={onSubmit}
+                className="row align-items-center py-4 px-2 booking-engine-form">
                 <div className="col col-12 col-lg-3">
                     <div className="d-flex flex-column gap-1 input-container">
-                        <label htmlFor="stay-input">Scegli il periodo</label>
                         <Calendar
-                            className="booking-engine-form-input"
                             name="stay-input"
                             id="stay-input"
-                            onChange={(e) => console.log(e.value)}
+                            onChange={stayInputChange}
                             selectionMode="range"
+                            placeholder="Periodo Soggiorno"
+                            dateFormat="dd/mm/yy"
                             readOnlyInput
                             hideOnRangeSelection
                         />
@@ -24,9 +35,13 @@ const BookingEngineForm = () => {
                 </div>
                 <div className="col col-12 col-lg-2">
                     <div className="d-flex flex-column gap-1 input-container">
-                        <label htmlFor="adults">Adulti</label>
-                        <select name="adults" id="adults">
-                            <option value="">Adulti</option>
+                        <select
+                            className="py-1 px-2 booking-engine-form-input"
+                            onChange={adultsChange}
+                            name="adults"
+                            id="adults"
+                        >
+                            <option value="0">Adulti</option>
                             <option value="1">1</option>
                             <option value="2">2</option>
                             <option value="3">3</option>
@@ -37,9 +52,12 @@ const BookingEngineForm = () => {
                 </div>
                 <div className="col col-12 col-lg-2">
                     <div className="d-flex flex-column gap-1 input-container">
-                        <label htmlFor="children">Bambini (3-17)</label>
-                        <select name="children" id="children">
-                            <option value="">Bambini</option>
+                        <select
+                            className="py-1 px-2 booking-engine-form-input"
+                            name="children"
+                            id="children"
+                        >
+                            <option value="0">Bambini (3-17 anni)</option>
                             <option value="1">1</option>
                             <option value="2">2</option>
                             <option value="3">3</option>
@@ -50,15 +68,23 @@ const BookingEngineForm = () => {
                 </div>
                 <div className="col col-12 col-lg-2">
                     <div className="d-flex flex-column gap-1 input-container">
-                        <label htmlFor="infant">Hai un neonato (0-2)?</label>
-                        <select name="infant" id="infant">
-                            <option value="false">No</option>
+                        <select
+                            className="py-1 px-2 booking-engine-form-input"
+                            name="infant"
+                            id="infant"
+                        >
+                            <option value="false">Neonato (0-2 anni)</option>
                             <option value="true">Si</option>
+                            <option value="false">No</option>
                         </select>
                     </div>
                 </div>
                 <div className="col col-12 col-lg-3">
-                    <button className="py-2 px-4 rounded rounded-2 check-availability-btn">CONTROLLA DISPONIBILITA'</button>
+                    <button
+                        type="submit"
+                        className="py-2 px-4 rounded rounded-2 check-availability-btn">
+                        CONTROLLA DISPONIBILITA'
+                    </button>
                 </div>
             </form>
         </>

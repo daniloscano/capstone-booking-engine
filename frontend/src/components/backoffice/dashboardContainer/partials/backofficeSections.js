14f9ed3b524da13cf31a81f7fa3018cc3add2ce4
Danilo Scano <daniloscano@gmail.com>
2025-07-21 02:46:54 +0200

    add: backoffice dashboard cards

---
+++ frontend/src/components/backoffice/dashboardContainer/partials/backofficeSections.js
@@ -0,0 +1,39 @@
+export const backofficeSections = [
+    {
+        title: 'Prenotazioni',
+        icon: '/icons/booking-icon.png',
+        iconAlt: 'booking icon',
+        href: '/backoffice/bookings'
+    },
+    {
+        title: 'Preventivi',
+        icon: '/icons/quote-request-icon.png',
+        iconAlt: 'quote request icon',
+        href: '/backoffice/quote-requests'
+    },
+    {
+        title: 'Tariffe',
+        icon: '/icons/rates-icon.png',
+        iconAlt: 'rates icon',
+        href: '/backoffice/rates'
+    },
+    {
+        title: 'Camere',
+        icon: '/icons/rooms-icon.png',
+        iconAlt: 'rooms icon',
+        href: '/backoffice/rooms'
+    },
+    {
+        title: 'Servizi',
+        icon: '/icons/services-icon.png',
+        iconAlt: 'ancillary icon',
+        href: '/backoffice/ancillaries'
+    },
+
+    {
+        title: 'Utenti',
+        icon: '/icons/users-icon.png',
+        iconAlt: 'users icon',
+        href: '/backoffice/users'
+    }
+]

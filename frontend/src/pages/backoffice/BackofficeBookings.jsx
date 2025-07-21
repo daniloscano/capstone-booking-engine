83051688799c4c0eebe94f774b61fb9d209f8512
Danilo Scano <daniloscano@gmail.com>
2025-07-21 02:57:36 +0200

    add: backoffice pages

---
+++ frontend/src/pages/backoffice/BackofficeBookings.jsx
@@ -0,0 +1,13 @@
+import BackofficeBaseLayout from "../../layouts/backoffice/BackofficeBaseLayout.jsx";
+
+const BackofficeBookings = () => {
+    return (
+        <>
+            <BackofficeBaseLayout>
+                <h1>Prenotazioni</h1>
+            </BackofficeBaseLayout>
+        </>
+    );
+};
+
+export default BackofficeBookings;

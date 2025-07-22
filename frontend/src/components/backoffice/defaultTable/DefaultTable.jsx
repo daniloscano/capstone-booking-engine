f2b65008cb79a60c3fe3008182e718fb11a69694
Danilo Scano <daniloscano@gmail.com>
2025-07-22 22:00:49 +0200

    add: actions to default table reusable component

--- frontend/src/components/backoffice/defaultTable/DefaultTable.jsx
+++ frontend/src/components/backoffice/defaultTable/DefaultTable.jsx
@@ -1,9 +1,15 @@
 import {useEffect} from "react";
 import {DataTable} from "primereact/datatable";
 import {Column} from "primereact/column";
+import {Pencil, Trash} from "lucide-react";
 import './defaultTable.css'
 
-const DefaultTable = ({ value, page, pageSize, setPage, setPageSize, totalValues, onFetch, columns }) => {
+const DefaultTable = ({
+    value, columns,
+    page, pageSize, setPage, setPageSize, totalValues,
+    showActions = false,
+    onFetch, onEdit, onDelete
+}) => {
     const onPaginationChange = (e) => {
         if (e.rows !== pageSize) setPageSize(e.rows)
         if (e.page !== page) setPage(e.page)
@@ -17,6 +23,8 @@ const DefaultTable = ({ value, page, pageSize, setPage, setPageSize, totalValues
         <>
             <DataTable
                 value={value}
+                scrollable
+                scrollHeight="600px"
                 paginator
                 lazy
                 rows={pageSize}
@@ -27,8 +35,31 @@ const DefaultTable = ({ value, page, pageSize, setPage, setPageSize, totalValues
             >
                 {
                     columns.map((column, index) => (
-                        <Column key={`column-${index}`} { ...column } />
+                        <Column key={`column-${index}`} {...column} />
                     ))
+                }
+                {
+                    showActions && (
+                        <Column
+                            header="Azioni"
+                            body={(rowData) => (
+                                <div className="d-flex justify-content-center align-items-center gap-2">
+                                    <button
+                                        onClick={(rowData) => onEdit(rowData._id)}
+                                        className="py-1 px-2 rounded rounded-2 edit-btn"
+                                    >
+                                        <Pencil />
+                                    </button>
+                                    <button
+                                        onClick={(rowData) => onDelete(rowData._id)}
+                                        className="py-1 px-2 rounded rounded-2 delete-btn"
+                                    >
+                                        <Trash />
+                                    </button>
+                                </div>
+                            )}
+                        />
+                    )
                 }
             </DataTable>
         </>

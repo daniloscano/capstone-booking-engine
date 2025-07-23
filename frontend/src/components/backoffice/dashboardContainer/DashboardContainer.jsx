a930bd5c8cefbff8ec66d159961d80609bdd4bad
Danilo Scano <daniloscano@gmail.com>
2025-07-23 22:15:32 +0200

    update: dashboard container to show username and role of user

--- frontend/src/components/backoffice/dashboardContainer/DashboardContainer.jsx
+++ frontend/src/components/backoffice/dashboardContainer/DashboardContainer.jsx
@@ -1,11 +1,16 @@
 import { backofficeSections } from "./partials/backofficeSections.js";
+import useSession from "../../../hooks/website/useSession.js";
 import DashboardCard from "./partials/DashboardCard.jsx";
 
 const DashboardContainer = () => {
+    const session = useSession()
+
     return (
         <>
             <section className="my-4 dashboard-container">
                 <div className="container">
+                    <h1>Benvenuto, {session.username}</h1>
+                    <p>Stai lavorando come <b>{session.role === 'admin' ? 'Amministratore' : 'Operatore' }</b></p>
                     <div className="row">
                         {
                             backofficeSections.map((section, index) => (

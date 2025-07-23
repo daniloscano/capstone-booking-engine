e460ca3988f5bf028aa8692dd9ea37926b63ec43
Danilo Scano <daniloscano@gmail.com>
2025-07-23 22:26:50 +0200

    add: services hook

---
+++ frontend/src/hooks/backoffice/useServices.js
@@ -0,0 +1,30 @@
+import useServicesStore from "../../stores/backoffice/useServicesStore.js";
+
+export const useServices = () => {
+    const { setServices, setServicesLoading, setServicesError, setTotalResults } = useServicesStore()
+    const token = localStorage.getItem('token')
+
+    const getAllServices = async (page, pageSize) => {
+        try {
+            setServicesLoading(true)
+            const response = await fetch(`${import.meta.env.VITE_SERVER_BASE_URL}/ancillaries?page=${page + 1}&pageSize=${pageSie}`, {
+                headers: {
+                    "Content-Type": 'application/json',
+                    "Authorization": JSON.parse(token)
+                }
+            })
+            const jsonResponse = await response.json()
+            setTotalResults(jsonResponse.ancillaries.totalResults)
+            setServices(jsonResponse.ancillaries.data)
+        } catch (err) {
+            setServicesLoading(false)
+            setServicesError(err)
+        } finally {
+            setServicesLoading(false)
+        }
+    }
+
+    return {
+        getAllServices
+    }
+}

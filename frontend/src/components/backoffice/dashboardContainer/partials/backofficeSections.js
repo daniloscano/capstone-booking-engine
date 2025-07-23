40dc46a575ad8323af9527cf2c62b1e050792272
Danilo Scano <daniloscano@gmail.com>
2025-07-24 00:28:38 +0200

    update: backoffice dashboard to show users only when logged user is admin

--- frontend/src/components/backoffice/dashboardContainer/partials/backofficeSections.js
+++ frontend/src/components/backoffice/dashboardContainer/partials/backofficeSections.js
@@ -1,30 +1,35 @@
 export const backofficeSections = [
     {
         title: 'Prenotazioni',
+        key: 'bookings',
         icon: '/icons/booking-icon.png',
         iconAlt: 'booking icon',
         href: '/backoffice/bookings'
     },
     {
         title: 'Preventivi',
+        key: 'quoteRquests',
         icon: '/icons/quote-request-icon.png',
         iconAlt: 'quote request icon',
         href: '/backoffice/quote-requests'
     },
     {
         title: 'Listini',
+        key: 'rates',
         icon: '/icons/rates-icon.png',
         iconAlt: 'rates icon',
         href: '/backoffice/rates'
     },
     {
         title: 'Camere',
+        key: 'rooms',
         icon: '/icons/rooms-icon.png',
         iconAlt: 'rooms icon',
         href: '/backoffice/rooms'
     },
     {
         title: 'Servizi',
+        key: 'services',
         icon: '/icons/services-icon.png',
         iconAlt: 'ancillary icon',
         href: '/backoffice/ancillaries'
@@ -32,6 +37,7 @@ export const backofficeSections = [
 
     {
         title: 'Utenti',
+        key: 'users',
         icon: '/icons/users-icon.png',
         iconAlt: 'users icon',
         href: '/backoffice/users'

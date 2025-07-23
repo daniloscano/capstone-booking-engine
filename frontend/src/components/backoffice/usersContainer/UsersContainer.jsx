fbd0f26f48fc245b8103df0676c6b430e7fb4386
Danilo Scano <daniloscano@gmail.com>
2025-07-23 23:50:26 +0200

    add: users container

---
+++ frontend/src/components/backoffice/usersContainer/UsersContainer.jsx
@@ -0,0 +1,13 @@
+const UsersContainer = () => {
+    return (
+        <>
+            <section className="users-section">
+                <div className="container-fluid p-4 my-4 users-container">
+                    <h2 className="mb-4">Utenti</h2>
+                </div>
+            </section>
+        </>
+    );
+};
+
+export default UsersContainer;

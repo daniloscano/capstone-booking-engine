33d717cbd36e0d1e47d383608604198459cdfcb5
Danilo Scano <daniloscano@gmail.com>
2025-07-24 01:14:57 +0200

    add: rates container

--- frontend/src/components/backoffice/ratesContainer/RatesContainer.jsx
+++ frontend/src/components/backoffice/ratesContainer/RatesContainer.jsx
@@ -1,9 +1,44 @@
+import useRatesStore from "../../../stores/backoffice/useRatesStore.js";
+import {useRates} from "../../../hooks/backoffice/useRates.js";
+import DefaultTable from "../defaultTable/DefaultTable.jsx";
+import {format} from "date-fns";
+
 const RatesContainer = () => {
+    const {
+        rates, ratesLoading, ratesError,
+        page, pageSize,
+        setPage, setPageSize,
+        totalResults
+    } = useRatesStore()
+    const { getAllRates } = useRates()
+
+    const columns = [
+        { field: "_id", header: "Codice" },
+        { field: "roomTypeId.name", header: "Camera" },
+        { field: "roomTypeId.type", header: "Tipologia" },
+        { field: "roomTypeId.category", header: "Categoria" },
+        { field: "date", header: "Data", body: row => format(row.date, 'dd/MM/yyyy') },
+        { header: "Importo", body: row => `€ ${row.price.toFixed(2)}`},
+        { header: "Adulto Extra", body: row => `${row.extraAdultMultiplier * 100} %` },
+        { header: "Bambino Extra", body: row => `${row.extraChildMultiplier * 100} %` },
+    ]
+
     return (
         <>
             <section className="rates-section">
                 <div className="container-fluid p-4 my-4 rates-container">
                     <h2 className="mb-4">Listini</h2>
+                    <DefaultTable
+                        value={rates}
+                        page={page}
+                        pageSize={pageSize}
+                        setPage={setPage}
+                        setPageSize={setPageSize}
+                        totalValues={totalResults}
+                        onFetch={getAllRates}
+                        columns={columns}
+                        showActions={true}
+                    />
                 </div>
             </section>
         </>
